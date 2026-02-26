e3c07a386863f7cd2c6ae703b8aa3edab372995b
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-02-26 20:37:43 -0300

    DTO añadidos

---
+++ src/main/java/com/bootcamp/dto/product/ProductResumeDTO.java
@@ -0,0 +1,45 @@
+package com.bootcamp.dto.product;
+
+import java.math.BigDecimal;
+
+/**
+ * DTO para manejar a los mejores productos
+ */
+public class ProductResumeDTO {
+    private Long id;
+    private String name;
+    private String shortDescription;
+    private String urlImage;
+    private BigDecimal price;
+
+    public ProductResumeDTO() {
+    }
+
+    public ProductResumeDTO(Long id, String name, String shortDescription, String urlImage, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.shortDescription = shortDescription;
+        this.urlImage = urlImage;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
